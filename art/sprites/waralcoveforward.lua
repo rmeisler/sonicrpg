b8e1032bd28f3cc4147d852257886e3d1925d486
DESKTOP-0VORAK7\Reggie <reggie.meisler@gmail.com>
2021-03-22 05:20:40 -0700

    Filling out war alcove structures (placement, inner, collision, art, etc)

---
+++ art/sprites/waralcoveforward.lua
@@ -0,0 +1,21 @@
+return {
+    image = "waralcoveforward.png",
+    starting = "right",
+    w = 450,
+    h = 300,
+	
+	inner_offsets = {
+		right = {668, -66},
+		left = {90, -66}
+	},
+
+    animations = {
+		right = {
+			frames = {{0,0}}
+		},
+		
+		left = {
+			frames = {{1,0}}
+		}
+    }
+}
