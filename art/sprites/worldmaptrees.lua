dc9c80db4729c04527f6084917e37d72bfe8cfc6
rmeisler <reggie.meisler@gmail.com>
2022-02-14 04:31:18 -0800

    Minor work on worldmap (Dark Swamp)
    Minor work on worldmap (Dark Swamp)

---
+++ art/sprites/worldmaptrees.lua
@@ -0,0 +1,15 @@
+return {
+    image = "worldmaptrees.png",
+    starting = "greatforest",
+    w = 110,
+    h = 50,
+
+    animations = {
+        greatforest = {
+            frames = {{0,0}}
+        },
+		darkswamp = {
+            frames = {{1,0}}
+        }
+    }
+}
