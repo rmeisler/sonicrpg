7d252987a343c0a7f650a460293cec052fb272de
rmeisler <reggie.meisler@gmail.com>
2022-05-24 05:38:32 -0700

    Cambot for Iron Lock
    Cambot for Iron Lock

---
+++ maps/castle1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="castle1" tilewidth="32" tileheight="32" tilecount="1050" columns="35">
+ <image source="../art/tiles/rotorworkshopcorner.png" trans="b326bd" width="1120" height="960"/>
+</tileset>
---
+++ maps/ironlock.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ironlock" tilewidth="32" tileheight="32" tilecount="7000" columns="70">
+ <image source="../art/tiles/ironlock.png" trans="904f94" width="2240" height="3200"/>
+</tileset>
