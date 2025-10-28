18aad5593ff940d152afeaae3c0963c0c92efc61
rmeisler <reggie.meisler@gmail.com>
2025-10-27 22:54:21 -0700

    More work on "save the terrapods" section
    More work on "save the terrapods" section

---
+++ art/sprites/terrapod.lua
@@ -0,0 +1,21 @@
+return {
+    image = "terrapod.png",
+    starting = "auntt",
+    w = 214,
+    h = 129,
+
+    animations = {
+        mamat = {
+            frames = {{0,0}}
+        },
+		auntt = {
+            frames = {{1,0}}
+        },
+		grampat = {
+            frames = {{2,0}}
+        },
+		papat = {
+            frames = {{3,0}}
+        },
+    }
+}
---
+++ art/sprites/terrapodcage.lua
@@ -0,0 +1,15 @@
+return {
+    image = "terrapodcage.png",
+    starting = "over",
+    w = 224,
+    h = 173,
+
+    animations = {
+        over = {
+            frames = {{0,0}}
+        },
+		under = {
+            frames = {{1,0}}
+        }
+    }
+}
