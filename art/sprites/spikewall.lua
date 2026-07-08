c90afe5380d5294448b8f17970b7f9ddc84c794d
rmeisler <reggie.meisler@gmail.com>
2026-07-08 16:17:00 -0700

    More work on intro section of part 1
    More work on intro section of part 1

---
+++ art/sprites/spikewall.lua
@@ -0,0 +1,15 @@
+return {
+    image = "spikewall.png",
+    starting = "left",
+    w = 50,
+    h = 123,
+
+    animations = {
+		left = {
+            frames = {{0,0}}
+        },
+		right = {
+            frames = {{1,0}}
+        }
+    }
+}
---
+++ maps/actions/ep6trapped2.lua
@@ -0,0 +1,47 @@
+return function(scene, hint)
+	local Transform = require "util/Transform"
+	local Rect = unpack(require "util/Shapes")
+	local Layout = require "util/Layout"
+
+	local Action = require "actions/Action"
+	local TypeText = require "actions/TypeText"
+	local Menu = require "actions/Menu"
+	local MessageBox = require "actions/MessageBox"
+	local Move = require "actions/Move"
+	local PlayAudio = require "actions/PlayAudio"
+	local Ease = require "actions/Ease"
+	local Parallel = require "actions/Parallel"
+	local Serial = require "actions/Serial"
+	local Executor = require "actions/Executor"
+	local Wait = require "actions/Wait"
+	local Repeat = require "actions/Repeat"
+	local Do = require "actions/Do"
+	local AudioFade = require "actions/AudioFade"
+	local Spawn = require "actions/Spawn"
+	local BlockPlayer = require "actions/BlockPlayer"
+	local Animate = require "actions/Animate"
+	local SpriteNode = require "object/SpriteNode"
+
+	scene.player.sprite.visible = false
+	scene.player.dropShadow.hidden = true
+
+	return BlockPlayer {
+		Do(function()
+			scene.player.sprite.visible = false
+			scene.player.dropShadow.hidden = true
+		end),
+		Wait(1),
+		scene.objectLookup.Antoine:hop(),
+		PlayAudio("sfx", "bang", 1, true),
+		Animate(scene.objectLookup.Antoine.sprite, "deadvisor"),
+		Wait(1.2),
+		
+		Ease(scene.camPos, "x", -1150, 0.3),
+		
+		MessageBox{message="Sally: Ugh! {p60}Where is that hedgehog!?"},
+		
+		Do(function()
+			--scene:changeScene{map="greatforest_ep6_intro1", fadeInSpeed=20, fadeOutSpeed=20, fadeOutMusic=false}
+		end)
+	}
+end
